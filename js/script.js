a2aa53055c2afb39d963bd733ceb8ac6dc02b799
Rahmadi Fadillah <rahmadifadillah83@gmail.com>
2023-04-12 21:26:48 +0700

    rework js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,51 @@
-function subcribe() {
-    name = document.forms["form1"]["email"].value;
+// Tambahkan kode JavaScript kalian di file ini
+function handleGetFormData() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const city = document.getElementById("city").value;
+    const zipCode = document.getElementById("zip-code").value;
+    const status = document.getElementById("status").checked;
 
-    if (name == "") {
-        alert("Please Enter Email")
+    return {
+        name,
+        email,
+        city,
+        zipCode,
+        status
+    };
+}
+
+function isNumber(input) {
+    return !isNaN(input);
+}
+
+function checkboxIsChecked() {
+    const checkbox = document.getElementById("status");
+
+    if (checkbox.checked) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateFormData(formData) {
+    if (
+        formData &&
+        isNumber(formData.zipCode) &&
+        checkboxIsChecked()
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function submit() {
+    if (validateFormData(handleGetFormData()) == false) {
+        return (document.getElementById("warning").innerHTML =
+            "Periksa form anda sekali lagi");
+    } else {
+        return (document.getElementById("warning").innerHTML = "");
     }
 }
